39695144df098d5092491d1de84f512dd54a2561
TommyAudet <tommyaudet@magame.ca>
2020-09-30 13:14:14 -0400

    animation fonctionnent, doit refractorer

--- script.js
+++ script.js
@@ -185,8 +185,8 @@ function hamburgerMaker(hamburgerName, containerName, checkBoxName, forCheckBoxN
         // parent.textContent = text;
         };
 
-        this.generateKeyframes = function(indx, from1, from2, from3, to1, to2, to3){
-            let animLine = this.hamburgerName + "Animate";
+        this.generateKeyframes = function(indx, state, from1, from2, from3, to1, to2, to3){
+            let animLine = this.hamburgerName + state;
             let defaultLine = this.hamburgerName + "Default";
             let text = 
             "@keyframes " + animLine + indx + "{" + "\n" +
@@ -194,27 +194,58 @@ function hamburgerMaker(hamburgerName, containerName, checkBoxName, forCheckBoxN
             "to{ " + to1 + to2 + to3 + " }" + "\n" +
             "}" + "\n" + "\n" 
             ;
+            this.cumulateCss(text);
 
-            let parent = document.getElementById("toCopy");
-            let br = document.createElement("br");
-            let br2 = document.createElement("br");
-            parent.appendChild(br);
-            parent.appendChild(br2);
-            let content = text;
-            parent.textContent += content;
-            this.generatedKeyframes += content;
         };
+
+        this.checkBoxChecked = function(indx, state){
+            if(state == "Animate"){
+                let animName = this.hamburgerName + state;
+
+                let text = 
+                "#" + this.checkBoxName + ":checked ~ #" + this.labelName + " .theBulletLine:nth-child(" + indx + "){" + "\n" +
+                    "animation : " + animName + indx + " .5s forwards;" + "\n" +
+                "}" + "\n" + "\n"
+                ;
+
+                this.cumulateCss(text);
+            } else{
+                let animName = this.hamburgerName + state;
+                let text = 
+                ".theBulletLine:nth-child(" + indx + "){" + "\n" +
+                    "animation: " + animName + indx + " .5s forwards;" + "\n" +
+                "}" + "\n" + "\n" 
+                ;
+                this.cumulateCss(text);
+            }
+
+        };
+
 
         this.createAnimation = function(){
             let generate = this.generatedKeyframes;
             console.log(generate);
 
             let style = document.createElement("style");
+            style.setAttribute("id", "generatedStyle");
             // style.innerHTML(generate);
             let textNode = document.createTextNode(generate);
             style.appendChild(textNode);
             document.head.appendChild(style);
-        }
+        };
+
+        this.cumulateCss = function(text){
+            let content = text;
+            this.generatedKeyframes += content;
+
+
+            let parent = document.getElementById("toCopy");
+            let br = document.createElement("br");
+            let br2 = document.createElement("br");
+            parent.appendChild(br);
+            parent.appendChild(br2);
+            parent.textContent += content;
+        };
 
 }
 
@@ -232,11 +263,24 @@ theBullet.setLineStyle(theBullet.line);
 theBullet.generateEmbededHtml();
 theBullet.generateEmbededAnimation();
 
-theBullet.generateKeyframes("1", "transform-origin: center;", "transform : rotate(0deg);", "", "transform-origin: center;", "transform:  translateY(200%) rotateZ(-45deg);", "");
-theBullet.generateKeyframes("2", "transform-origin: center;", "transform : translateX(0);", "opacity: 1;", "transform-origin: center;", "transform : translateX(-100%);", "opacity: 0;");
-theBullet.generateKeyframes("3", "transform-origin: center;", "transform : rotate(0deg);", "", "transform-origin: center;", "transform : translateY(-200%) rotateZ(45deg);", "");
+theBullet.generateKeyframes("1","Animate", "transform-origin: center;", "transform : rotate(0deg);", "", "transform-origin: center;", "transform:  translateY(200%) rotateZ(-45deg);", "");
+theBullet.generateKeyframes("2","Animate", "transform-origin: center;", "transform : translateX(0);", "opacity: 1;", "transform-origin: center;", "transform : translateX(-100%);", "opacity: 0;");
+theBullet.generateKeyframes("3","Animate", "transform-origin: center;", "transform : rotate(0deg);", "", "transform-origin: center;", "transform : translateY(-200%) rotateZ(45deg);", "");
 
+theBullet.generateKeyframes("1","Initial", "transform-origin: center;",  "transform:  translateY(200%) rotateZ(-45deg);", "",  "transform-origin: center;", "transform:  translateY(0) rotateZ(0deg);", "");
+theBullet.generateKeyframes("2","Initial", "transform-origin: center;", "transform : translateX(-100%); opacity: 0;", "", "transform-origin: center;", "transform:  translateY(0) rotateZ(0deg);", "");
+theBullet.generateKeyframes("3","Initial", "transform-origin: center;",  "transform:  translateY(-200%) rotateZ(45deg);", "", "transform-origin: center;", "transform:  translateY(0) rotateZ(0deg);", "");
 
+theBullet.checkBoxChecked("1", "Animate");
+theBullet.checkBoxChecked("2", "Animate");
+theBullet.checkBoxChecked("3", "Animate");
+
+theBullet.checkBoxChecked("1", "Initial");
+theBullet.checkBoxChecked("2", "Initial");
+theBullet.checkBoxChecked("3", "Initial");
 
 theBullet.createAnimation();
+
+
+
 
